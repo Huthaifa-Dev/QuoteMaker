bcbfa89d8f919dad2670866fdb97da5bc716ee0d
Huthaifa-Dev <huthaifajamal2@gmail.com>
2022-05-06 20:16:32 +0300

    added lazy loading

---
+++ build/static/js/6.6cc35bc4.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 6.6cc35bc4.chunk.js.LICENSE.txt */
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[6],{34:function(e,t,c){"use strict"}}]);
+//# sourceMappingURL=6.6cc35bc4.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from "react";
-import { Link, Navigate, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./Pages/AllQuotes";
-import NewQuote from "./Pages/NewQuote";
-import QuoteDetail from "./Pages/QuoteDetail";
-import ErrorPage from "./Pages/ErrorPage";
+// import NewQuote from "./Pages/NewQuote";
+// import QuoteDetail from "./Pages/QuoteDetail";
+// import ErrorPage from "./Pages/ErrorPage";
 import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
-
+const NewQuote = React.lazy(() => import('./Pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./Pages/QuoteDetail'))
+const ErrorPage = React.lazy(() => import('./Pages/ErrorPage'))
 function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} exact />
-        <Route path="quotes" element={<AllQuotes />} exact />
-        <Route path="quotes/:quoteId" element={<QuoteDetail />}>
-          <Route path={`comments`} element={<Comments />} />
-        </Route>
-        <Route path="new-quote" element={<NewQuote />} exact />
-        <Route path="*" element={<ErrorPage />} exact />
+      <Suspense fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      } >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} exact />
+          <Route path="quotes" element={<AllQuotes />} exact />
+          <Route path="quotes/:quoteId" element={<QuoteDetail />}>
+            <Route path={`comments`} element={<Comments />} />
+          </Route>
+          <Route path="new-quote" element={<NewQuote />} exact />
+          <Route path="*" element={<ErrorPage />} exact />
 
-      </Routes>
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
